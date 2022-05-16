ceda3f7bbdce910dbeba9833a09c60bf62561ab1
Vladimir <glen_90@bk.ru>
2022-05-16 23:44:18 +0300

    Second part of the second lab

---
+++ cSharpCourse/secondLab/vladimirGlinskii.csCourse.secondLab_Struct/vladimirGlinskii.csCourse.secondLab_Struct/Struct.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vladimirGlinskii.csCourse.secondLab_Struct
+{
+    public enum AccountType { Checking, Deposit }
+    public struct BankAccount
+    {
+        public long accNumber;
+        public decimal accBalance; 
+        public AccountType accType;
+
+
+    }
+    internal class Struct
+    {
+        static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBalance = (decimal)3200.00;
+            goldAccount.accNumber = 123;
+
+            Console.WriteLine("***Account Summary***");
+            Console.WriteLine("Account Number {0}", goldAccount.accNumber);
+            Console.WriteLine("Account Type {0}", goldAccount.accType);
+            Console.WriteLine("Account Balance {0}", goldAccount.accBalance);
+
+            Console.ReadLine(); 
+        }
+    }
+}
