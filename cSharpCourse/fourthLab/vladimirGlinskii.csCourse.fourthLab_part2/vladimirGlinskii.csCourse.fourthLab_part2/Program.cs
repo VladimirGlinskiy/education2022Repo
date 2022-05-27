279407e364aadc4466bf0eefc420a17454cfd6c7
Vladimir <glen_90@bk.ru>
2022-05-27 22:11:10 +0300

    next homework

---
+++ cSharpCourse/fourthLab/vladimirGlinskii.csCourse.fourthLab_part2/vladimirGlinskii.csCourse.fourthLab_part2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace vladimirGlinskii.csCourse.fourthLab_part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int x = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int y = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Переменные до перестановки \n" +
+                "x: {0}, y: {1}", x, y);
+            Utils.Swap(ref x, ref y);
+            Console.WriteLine("Переменные после перестановки \n" +
+                "x: {0}, y: {1}", x, y);
+            Console.ReadLine();
+        }
+    }
+}
