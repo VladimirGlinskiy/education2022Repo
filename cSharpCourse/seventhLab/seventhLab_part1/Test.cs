3af8366789ee80acd168dd49069d36e0459ecf49
Vladimir Glinskiy <glen_90@bk.ru>
2022-06-03 04:56:26 +0300

    Seventh lab
    Seventh lab

---
+++ cSharpCourse/seventhLab/seventhLab_part1/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace seventhLab_part1
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            decimal transferAmount = 10;
+
+            BankAccount b1 = new BankAccount();
+            b1.Populate(100);
+
+            BankAccount b2 = new BankAccount();
+            b2.Populate(100);
+
+            Console.WriteLine("Перед переводом");
+            Console.WriteLine("Первый счет. Тип: {0}, Номер: {1}, Баланс: {2} ", b1.Type(), b1.Number(), b1.Balance());
+            Console.WriteLine("Второй счет. Тип: {0}, Номер: {1}, Баланс: {2} ", b2.Type(), b2.Number(), b2.Balance());
+            b1.TransferForm(b2, transferAmount);
+            Console.WriteLine("После перевода {0}", transferAmount);
+            Console.WriteLine("Первый счет. Тип: {0}, Номер: {1}, Баланс: {2} ", b1.Type(), b1.Number(), b1.Balance());
+            Console.WriteLine("Второй счет. Тип: {0}, Номер: {1}, Баланс: {2} ", b2.Type(), b2.Number(), b2.Balance());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cSharpCourse/seventhLab/seventhLab_part2/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Utils
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите строку для разворота");
+            string s = Console.ReadLine();
+            Utils.Reverse(ref s);
+            Console.WriteLine("Строка после разворота");
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
