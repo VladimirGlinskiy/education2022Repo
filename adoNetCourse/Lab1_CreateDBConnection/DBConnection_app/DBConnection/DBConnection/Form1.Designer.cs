3b04427dad7ea6abfb4e63524fca05c6f36ebbe3
Vladimir <glen_90@bk.ru>
2022-09-10 03:23:09 +0300

    exersise 1-5. Connection-App

---
+++ adoNetCourse/Lab1_CreateDBConnection/DBConnection_app/DBConnection/DBConnection/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace DBConnection
+{
+    partial class Form1
+    {
+        
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.connectToDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hostsListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToDatabaseToolStripMenuItem,
+            this.disconnectDatabaseToolStripMenuItem,
+            this.hostsListToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // connectToDatabaseToolStripMenuItem
+            // 
+            this.connectToDatabaseToolStripMenuItem.Name = "connectToDatabaseToolStripMenuItem";
+            this.connectToDatabaseToolStripMenuItem.Size = new System.Drawing.Size(129, 20);
+            this.connectToDatabaseToolStripMenuItem.Text = "Connect to Database";
+            this.connectToDatabaseToolStripMenuItem.Click += new System.EventHandler(this.connectToDatabaseToolStripMenuItem_Click);
+            // 
+            // disconnectDatabaseToolStripMenuItem
+            // 
+            this.disconnectDatabaseToolStripMenuItem.Name = "disconnectDatabaseToolStripMenuItem";
+            this.disconnectDatabaseToolStripMenuItem.Size = new System.Drawing.Size(128, 20);
+            this.disconnectDatabaseToolStripMenuItem.Text = "Disconnect database";
+            this.disconnectDatabaseToolStripMenuItem.Click += new System.EventHandler(this.disconnectDatabaseToolStripMenuItem_Click);
+            // 
+            // hostsListToolStripMenuItem
+            // 
+            this.hostsListToolStripMenuItem.Name = "hostsListToolStripMenuItem";
+            this.hostsListToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.hostsListToolStripMenuItem.Text = "Hosts List";
+            this.hostsListToolStripMenuItem.Click += new System.EventHandler(this.hostsListToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem connectToDatabaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectDatabaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hostsListToolStripMenuItem;
+    }
+}
+
